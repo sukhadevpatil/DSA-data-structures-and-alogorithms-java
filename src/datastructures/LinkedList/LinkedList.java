330befae88847ffa84a6215834e317062f9e8d1f
Sukhdev Patil <sukhadev.patil@gmail.com>
2024-06-17 00:04:12 +0530

    LinkedList changes

--- src/datastructures/LinkedList/LinkedList.java
+++ src/datastructures/LinkedList/LinkedList.java
@@ -39,7 +39,71 @@ public class LinkedList {
         } else {
             tail.next = newNode;
             tail = newNode;
+            length ++;
         }
+    }
+
+    public Node removeLast() {
+        if(length == 0) return null;
+
+        Node pre = head;
+        Node temp = head;
+
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+
+        tail = pre;
+        tail.next = null;
+
+        length--;
+
+        if(length == 0) {
+            head = null;
+            tail = null;
+        }
+
+        return temp;
+    }
+
+    public void prepend(int val) {
+        Node newNode = new Node(val);
+        if(length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst() {
+        if(length == 0) return null;
+
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+
+        if(length ==0) {
+            tail = null;
+        }
+
+        return temp;
+    }
+
+    public Node get(int index) {
+
+        if(index < 0 || index > length) return null;
+
+        Node temp = head;
+        for(int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+
+        return temp;
     }
 
     public Node getHead() {
--- src/datastructures/LinkedList/LinkedListMain.java
+++ src/datastructures/LinkedList/LinkedListMain.java
@@ -3,11 +3,54 @@ package datastructures.LinkedList;
 public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList(4);
+        linkedList.append(2);
+        linkedList.append(1);
 
         linkedList.getHead();
         linkedList.getTail();
         linkedList.getLength();
 
         linkedList.printList();
+
+        //Remove last item 4
+        System.out.println(linkedList.removeLast().value);
+
+        //Remove last item 2
+        System.out.println(linkedList.removeLast().value);
+
+        //Remove last item 1
+        System.out.println(linkedList.removeLast().value);
+
+        //null value
+        System.out.println(linkedList.removeLast());
+
+        System.out.println("=======================");
+        linkedList.prepend(1);
+        linkedList.prepend(2);
+
+        linkedList.printList();
+        System.out.println("=======================");
+
+        //linkedList.printList();
+
+        // Removed 1st element
+        System.out.println(linkedList.removeFirst().value);
+
+        // Removed 2nd element
+        System.out.println(linkedList.removeFirst().value);
+
+        //removes last element
+        System.out.println(linkedList.removeFirst());
+
+        System.out.println("==========================");
+        //linkedList.printList();
+        linkedList.prepend(33);
+        linkedList.prepend(44);
+        linkedList.prepend(55);
+        linkedList.printList();
+        System.out.println("=-------------------=");
+        System.out.println(linkedList.get(1).value);
+        System.out.println(linkedList.get(0).value);
+        System.out.println(linkedList.get(2).value);
     }
 }
